a6cc64996db3bcb142320f807fe80e81dba1ef70
Rahman Rejepov <rahmanrejepov777@gmail.com>
2023-12-23 14:39:31 +0300

    Domain Entity constructions completed

--- src/main/java/com/example/amadeus_project_case_study/domains/Airport.java
+++ src/main/java/com/example/amadeus_project_case_study/domains/Airport.java
@@ -1,4 +1,22 @@
 package com.example.amadeus_project_case_study.domains;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Airport {
+    @Column
+    @Id
+    private Long ID;
+    @Column
+    private String city;
 }
--- src/main/java/com/example/amadeus_project_case_study/domains/Flight.java
+++ src/main/java/com/example/amadeus_project_case_study/domains/Flight.java
@@ -1,4 +1,37 @@
 package com.example.amadeus_project_case_study.domains;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Flight {
+    @Column
+    @Id
+    private Long ID;
+
+    @OneToOne
+    @JoinColumn(name = "departureAirportId", referencedColumnName = "id")
+    private Airport departureAirport;
+
+    @OneToOne
+    @JoinColumn(name = "arrivalAirportId", referencedColumnName = "id")
+    private Airport arrivalAirport;
+
+    @Column
+    private LocalDateTime departureTime;
+
+    @Column
+    private LocalDateTime returnTime;
+
+    @Column
+    private Integer price;
 }
