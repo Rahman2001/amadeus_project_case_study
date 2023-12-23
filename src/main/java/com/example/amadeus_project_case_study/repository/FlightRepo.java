460b944ff64d011c295e32022eb226472d769319
Rahman Rejepov <rahmanrejepov777@gmail.com>
2023-12-23 17:07:02 +0300

    Unnecessary classes deleted (User, UserRepo)

--- src/main/java/com/example/amadeus_project_case_study/repository/FlightRepo.java
+++ src/main/java/com/example/amadeus_project_case_study/repository/FlightRepo.java
@@ -1,4 +1,21 @@
 package com.example.amadeus_project_case_study.repository;
 
-public interface FlightRepo {
+import com.example.amadeus_project_case_study.domains.Flight;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface FlightRepo extends JpaRepository<Flight, Long> {
+    @Query(value = "select * from flight where departure_airport_id = ?1 and arrival_airport_id = ?2 and " +
+            "departure_time = ?3 and return_time = ?4",nativeQuery = true)
+    List<Flight> getFlightsBy(Integer depAirId, Integer arrAirId,
+                              LocalDateTime depTime, LocalDateTime retTime);
+
+    @Query(value = "select * from flight where departure_airport_id = ?1 and arrival_airport_id = ?2 and " +
+            "departure_time = ?3",nativeQuery = true)
+    List<Flight> getFlightsBy(Integer depAirId, Integer arrAirId,
+                              LocalDateTime depTime);
+
 }
--- src/main/java/com/example/amadeus_project_case_study/services/UserService.java
+++
@@ -1,4 +0,0 @@
-package com.example.amadeus_project_case_study.services;
-
-public class UserService {
-}
