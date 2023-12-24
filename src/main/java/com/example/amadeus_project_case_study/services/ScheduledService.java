85a269c802e8948d67bed3ba8e98a89a6d7857d8
Rahman Rejepov <rahmanrejepov777@gmail.com>
2023-12-24 18:18:35 +0300

    Scheduled service is implemented

--- src/main/java/com/example/amadeus_project_case_study/services/ScheduledService.java
+++ src/main/java/com/example/amadeus_project_case_study/services/ScheduledService.java
@@ -1,4 +1,27 @@
 package com.example.amadeus_project_case_study.services;
 
+import com.example.amadeus_project_case_study.config.RetrofitClientConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+
+
+@Component
+@Slf4j
 public class ScheduledService {
+
+    private RetrofitClientConfig retrofitClientConfig;
+
+    @Autowired
+    public ScheduledService(RetrofitClientConfig retrofitClientConfig) {
+        this.retrofitClientConfig = retrofitClientConfig;
+    }
+
+    @Scheduled(cron = "0/7 * * * * *") //each 7 seconds  //to schedule for each day, write * * * 0/1 * *
+    public void scheduleRequest() {
+        log.info(LocalTime.now() + " scheduled job completed");
+    }
 }
